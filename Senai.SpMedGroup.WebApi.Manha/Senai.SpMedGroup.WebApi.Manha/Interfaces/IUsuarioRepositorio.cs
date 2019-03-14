997a5a8f16f224e6ec8d0f7fc5cad4417697f310
moralesba <42540259+moralesba@users.noreply.github.com>
2019-03-14 11:35:55 -0300

    Add files via upload

---
+++ Senai.SpMedGroup.WebApi.Manha/Senai.SpMedGroup.WebApi.Manha/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Senai.SpMedGroup.WebApi.Manha.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.SpMedGroup.WebApi.Manha.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        List<Usuario> Listar();
+        Usuario BuscarEmailSenha(string email, string senha);
+        Usuario BuscarUsuario(int usuarioId);
+        void Cadastrar(Usuario usuario);
+        void Alterar(Usuario usuario);
+        void Deletar(int id);
+    }
+}
---
+++ Senai.SpMedGroup.WebApi.Manha/Senai.SpMedGroup.WebApi.Manha/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Senai.SpMedGroup.WebApi.Manha.Context;
+using Senai.SpMedGroup.WebApi.Manha.Domains;
+using Senai.SpMedGroup.WebApi.Manha.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Senai.SpMedGroup.WebApi.Manha.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private string StringConexao = "Data Source=.\\SqlExpress; initial catalog = SpMedGroup; user id=sa; pwd=132";
+
+        public List<Usuario> Listar()
+        {
+            using (SpMedGroupContext ctx = new SpMedGroupContext())
+            {
+                return ctx.Usuario.ToList();
+            }
+        }
+
+        public void Cadastrar(Usuario usuario)
+        {
+            using (SpMedGroupContext ctx = new SpMedGroupContext())
+            {
+                ctx.Usuario.Add(usuario);
+                ctx.SaveChanges();
+            }
+        }
+
+        public Usuario BuscarEmailSenha(string email, string senha)
+        {
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                string QuerySelect = "select id_usuario, email, senha, tipo_usuario from usuario where email = @email and senha = @senha";
+
+                using (SqlCommand cmd = new SqlCommand(QuerySelect, con))
+                {
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@senha", senha);
+
+                    con.Open();
+
+                    SqlDataReader sdr = cmd.ExecuteReader();
+
+                    if (sdr.HasRows)
+                    {
+                        Usuario usuario = new Usuario();
+
+                        while (sdr.Read())
+                        {
+                            usuario.IdUsuario = Convert.ToInt32(sdr["id_usuario"]);
+                            usuario.Email = sdr["email"].ToString();
+                            usuario.TipoUsuario = Convert.ToInt16(sdr["tipo_usuario"]);
+                        }
+                        return usuario;
+                    }
+                }
+                return null;
+            }
+        }
+        public void Deletar(int id)
+            {
+                using (SpMedGroupContext ctx = new SpMedGroupContext())
+                {
+                    ctx.Usuario.Remove(ctx.Usuario.Find(id));
+                    ctx.SaveChanges();
+                }
+            }
+
+            public void Alterar(Usuario usuario)
+            {
+                using (SpMedGroupContext ctx = new SpMedGroupContext())
+                {
+                    Usuario usuarioExiste = ctx.Usuario.Find(usuario.IdUsuario);
+
+                    usuarioExiste.IdUsuario = usuario.IdUsuario;
+                    ctx.Usuario.Update(usuario);
+                    ctx.SaveChanges();
+                }
+            }
+
+        public Usuario BuscarUsuario(int usuarioId)
+        {
+            Usuario usuarioBuscado = new Usuario();
+
+            using (SpMedGroupContext ctx = new SpMedGroupContext())
+            {
+                usuarioBuscado = ctx.Usuario.Find(usuarioId);
+            }
+
+            return usuarioBuscado;
+        }
+    }
+}
+
+
